29248fcc0aade7076af3cf36d84cbfb282e69252
jepegit <jepe@ife.no>
2022-10-05 22:19:09 +0200

    try implement pre hook to check version

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,2 +1,20 @@
 import os
+import re
+import sys
+
+
+MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
+
+module_name = '{{ cookiecutter.module_name }}'
+cellpy_version = '{{ cookiecutter.cellpy_version }}'
+
+print(module_name)
+print(cellpy_version)
+
+if not re.match(MODULE_REGEX, module_name):
+    print('ERROR: %s is not a valid Python module name!' % module_name)
+
+    # exits with status 1 to indicate failure
+    sys.exit(1)
+
 print(f"setting up project in the following directory: {os.getcwd()}")
